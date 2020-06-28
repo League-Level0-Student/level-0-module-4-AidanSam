16af5e489d1b896716af4c9ec4b646762a078f38
Aidan <Aidan@AidanPC>
2020-06-28 14:45:32 -0700

    Random shit

---
+++ src/Minecraft/Optimizer.java
@@ -0,0 +1,19 @@
+package Minecraft;
+
+import javax.swing.JOptionPane;
+
+public class Optimizer {
+
+String beep = JOptionPane.showInputDialog("What is your swords damage?");
+int num = Integer.parseInt(beep);
+
+String critdmg = JOptionPane.showInputDialog("What is your swords strength?");
+int yum = Integer.parseInt(critdmg);
+
+String strength = JOptionPane.showInputDialog("What is your swords strength?");
+int bum = Integer.parseInt(strength);
+
+JOptionPane.showMessageDialog(null, "Nice job!");
+
+
+}
--- src/_04_test_scores/Test.java
+++ src/_04_test_scores/Test.java
@@ -4,17 +4,20 @@ import javax.swing.JOptionPane;
 
 public class Test {
 public static void main(String[] args) {
-	String quantity = JOptionPane.showInputDialog("What score did you get on your test? (Just the number)");
+	String beep = JOptionPane.showInputDialog("What is your swords damage?");
+	int num = Integer.parseInt(beep);
 	
-	int num = Integer.parseInt(quantity);
+	String combat = JOptionPane.showInputDialog("What is your combat level?");
+	int damage = Integer.parseInt(combat);
+	int dps = ((damage*4)+ 100)/100;
+	System.out.println(dps);
 	
-	if (num>80) {
-		JOptionPane.showMessageDialog(null, "Nice job!");
-	}else if(num>60){
-		JOptionPane.showMessageDialog(null, "You could study more.");
-	}else if(num<61){
-		JOptionPane.showMessageDialog(null, "Mcdonalds is hiring.");
-		
+	String critdmg = JOptionPane.showInputDialog("What is your swords crit damage?");
+	int yum = Integer.parseInt(critdmg);
+
+	String strength = JOptionPane.showInputDialog("What is your swords strength?");
+	int bum = Integer.parseInt(strength);
+	JOptionPane.showMessageDialog(null, "On a crit you will deal " + (dps *(((num + 5 + (bum/5)) * (1 + (bum/100))) * (yum/100))));
 	}
 }
-}
+
