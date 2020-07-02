bb08236a57b9ff33d68815717c1214a0a80713f9
Aidan <Aidan@AidanPC>
2020-07-01 18:28:32 -0700

    I did ore stuff

--- src/Minecraft/Optimizer.java
+++ src/Minecraft/Optimizer.java
@@ -10,10 +10,9 @@ int num = Integer.parseInt(beep);
 String critdmg = JOptionPane.showInputDialog("What is your swords strength?");
 int yum = Integer.parseInt(critdmg);
 
-String strength = JOptionPane.showInputDialog("What is your swords strength?");
+String strength = JOptionPane.showInputDialog("What hgfrytdhydgfis your swords strength?");
 int bum = Integer.parseInt(strength);
 
-JOptionPane.showMessageDialog(null, "Nice job!");
 
 
 }
